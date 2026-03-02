b5817a276bcb9ddd393abffb3a23ed199894f9af
Ndang11 <royaltyhopsin.com>
2026-03-02 14:38:54 +0100

    fixing linter errors

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-const searchBtn = document.getElementById("searchBtn");
-const searchInput = document.getElementById("searchInput");
-const results = document.getElementById("results");
+const searchBtn = document.getElementById('search-btn')
+const searchInput = document.getElementById('search-input')
+const results = document.getElementById('results')
 
-searchBtn.addEventListener("click", searchUser);
+searchBtn.addEventListener('click', searchUser)
 
-async function searchUser() {
-  const query = searchInput.value.trim();
+async function searchUser () {
+  const query = searchInput.value.trim()
 
-  if (!query) return;
+  if (!query) return
 
-  results.innerHTML = "Loading...";
+  results.innerHTML = 'Loading...'
 
   try {
-    const res = await fetch(`https://api.github.com/search/users?q=${query}`);
-    const data = await res.json();
+    const res = await fetch(`https://api.github.com/search/users?q=${query}`)
+    const data = await res.json()
 
-    results.innerHTML = "";
+    results.innerHTML = ''
 
     if (data.items.length === 0) {
-      results.innerHTML = "No users found";
-      return;
+      results.innerHTML = 'No users found'
+      return
     }
 
     for (const user of data.items) {
       const profileRes = await fetch(
-        `https://api.github.com/users/${user.login}`,
-      );
-      const profile = await profileRes.json();
+        `https://api.github.com/users/${user.login}`
+      )
+      const profile = await profileRes.json()
 
-      const div = document.createElement("div");
-      div.classList.add("user");
+      const div = document.createElement('div')
+      div.classList.add('user')
 
       div.innerHTML = `
         <img src="${profile.avatar_url}" class="avatar" />
 
         <h3>${profile.name || profile.login}</h3>
 
-        <p>${profile.bio || "No bio available"}</p>
+        <p>${profile.bio || 'No bio available'}</p>
 
         <p>
           Followers: ${profile.followers} |
@@ -44,15 +44,15 @@ async function searchUser() {
         </p>
 
         <small>Repos: ${profile.public_repos}</small>
-      `;
-      div.addEventListener("click", () => {
-        window.open(profile.html_url, "_blank");
-      });
+      `
+      div.addEventListener('click', () => {
+        window.open(profile.html_url, '_blank')
+      })
 
-      results.appendChild(div);
+      results.appendChild(div)
     }
   } catch (error) {
-    results.innerHTML = "Error fetching users";
-    console.error(error);
+    results.innerHTML = 'Error fetching users'
+    console.error(error)
   }
 }
